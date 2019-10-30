655ec2d6c4c2662fe7b5a3b48a82f75dbe5ee6b3
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2019-10-30 11:47:38 +0100

    update configure.js

--- configure.js
+++ configure.js
@@ -5,10 +5,19 @@ const skipPrompt = process.env.NO_INTERACTIVE || process.env.NO_PROMPT ? true : 
 const skipAutoconf = process.env.NO_AUTOCONF ? true : false;
 
 const generate = () => {
-  console.log(`writing index.js`);
+  const indexPath = `${__dirname}/../../index.js`;
+  const servicesPath = `${__dirname}/../../services`;
+  // creating entrypoint
+  console.log(`creating entry point (index.js)`);
+  if (!fs.existsSync(indexPath)) {
+    fs.writeFileSync(indexPath, 'require("backkit-kernel")({appdir: __dirname})');
+  }
 
-  if (!fs.existsSync(`${__dirname}/../../index.js`)) {
-    fs.writeFileSync(`${__dirname}/../../index.js`, 'require("backkit-kernel")({appdir: __dirname})');
+  // creating service dir
+  console.log(`creating services folder`);
+  if (!fs.existsSync(servicesPath)) {
+    fs.mkdirSync(servicesPath, {recursive: true});
+    fs.writeFileSync(`${servicesPath}/.gitkeep`, '');
   }
 };
 
